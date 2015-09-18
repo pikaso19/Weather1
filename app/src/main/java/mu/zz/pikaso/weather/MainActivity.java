9e6392e76658110e3b1b56c0fc497ed5b42d62c8
i3odja <wbodjaw@gmail.com>
2015-09-18 17:09:35 +0300

    comment bodja
    15 row

--- app/src/main/java/mu/zz/pikaso/weather/MainActivity.java
+++ app/src/main/java/mu/zz/pikaso/weather/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//One more comment, from copy of project
+	// from another user (bodja)
     }
 
 
